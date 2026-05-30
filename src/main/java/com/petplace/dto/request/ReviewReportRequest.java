0efc374b3291fbd1c4653ae39c471b09c159e897
TerrainCorn <ch2ice@gmail.com>
2026-05-30 20:00:33 +0900

    fix: ReviewReportRequest @Setter 추가 - Jackson 역직렬화 실패로 @NotBlank 검증 오류 수정

--- src/main/java/com/petplace/dto/request/ReviewReportRequest.java
+++ src/main/java/com/petplace/dto/request/ReviewReportRequest.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Schema(description = "리뷰 신고 요청 DTO")
 public class ReviewReportRequest {
